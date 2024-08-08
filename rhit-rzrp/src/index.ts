cbb9b71a2867148326a67741e300cc62fd2ccbce
AslamThachapalli <aslam.develop912@gmail.com>
2024-08-09 00:58:44 +0530

    verify payment endpoint

--- rhit-rzrp/src/index.ts
+++ rhit-rzrp/src/index.ts
@@ -1,24 +1,32 @@
 import { Hono } from 'hono'
 import { Buffer } from "buffer";
-import { env } from 'hono/adapter';
 import { z } from 'zod';
 import { validator } from 'hono/validator';
+import { createHmac } from 'node:crypto';
+import { cors } from 'hono/cors';
 
 // Encode string to base64token format.
 const encode = (str: string): string => Buffer.from(str, 'binary').toString('base64');
 
-const app = new Hono()
+type Bindings = {
+  RZRP_KEY_ID: string,
+  RZRP_KEY_SECRET: string
+}
 
-const schema = z.object({
+const app = new Hono<{ Bindings: Bindings }>()
+
+app.use('*', cors());
+
+const orderSchema = z.object({
   amount: z.number(),
   receipt: z.string().optional(),
   notes: z.record(z.string(), z.string()).optional(),
 })
 
 app.post(
-  '/order',
+  '/create-order',
   validator('json', (value, c) => {
-    const parsed = schema.safeParse(value);
+    const parsed = orderSchema.safeParse(value);
     if (!parsed.success) {
       return c.text('Invalid!', 401)
     }
@@ -32,14 +40,12 @@ app.post(
       'currency': 'INR'
     }
 
-    const { RZRP_KEY_ID, RZRP_KEY_SECRET } = env<{ RZRP_KEY_ID: string, RZRP_KEY_SECRET: string }>(c);
-
     let res = await fetch('https://api.razorpay.com/v1/orders', {
       method: 'POST',
       body: JSON.stringify(reqBody),
       headers: {
         "Content-Type": "application/json",
-        'Authorization': `Basic ${encode(`${RZRP_KEY_ID}:${RZRP_KEY_SECRET}`)}`
+        'Authorization': `Basic ${encode(`${c.env.RZRP_KEY_ID}:${c.env.RZRP_KEY_SECRET}`)}`
       }
     });
 
@@ -47,5 +53,34 @@ app.post(
 
     return c.json(jsn)
   })
+
+const toValidateSchema = z.object({
+  orderId: z.string(),
+  paymentId: z.string(),
+  signature: z.string(),
+})
+
+app.post(
+  '/verify-payment', 
+  validator('json', (value, c) => {
+    const parsed = toValidateSchema.safeParse(value);
+    if (!parsed.success) {
+      return c.text('Invalid!', 401)
+    }
+    return parsed.data
+  }),
+  async (c) => {
+  const { orderId, paymentId, signature } = c.req.valid('json');
+
+  const hmac = createHmac('sha256', c.env.RZRP_KEY_SECRET);
+
+  hmac.update(orderId + "|" + paymentId);
+
+  const generatedHash = hmac.digest('hex')
+
+  const isValid = generatedHash == signature
+
+  return c.json({ isValid });
+});
 
 export default app
